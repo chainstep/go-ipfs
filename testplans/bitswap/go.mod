32e67b321b2289919e7e88897ccbf4b124a4eb3e
Cory Schwartz <cory@protocol.ai>
2021-08-12 17:49:38 -0700

    bitswap

---
+++ testplans/bitswap/go.mod
@@ -0,0 +1,28 @@
+module github.com/ipfs/go-ipfs/testplans/example
+
+go 1.16
+
+require (
+	github.com/gopherjs/gopherjs v0.0.0-20190812055157-5d271430af9f // indirect
+	github.com/ipfs/go-bitswap v0.3.4
+	github.com/ipfs/go-block-format v0.0.3
+	github.com/ipfs/go-datastore v0.4.5
+	github.com/ipfs/go-ipfs-blockstore v1.0.4
+	github.com/ipfs/go-ipfs-exchange-interface v0.0.1
+	github.com/ipfs/go-ipns v0.1.0 // indirect
+	github.com/ipld/go-ipld-prime v0.11.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
+	github.com/libp2p/go-libp2p v0.14.3
+	github.com/libp2p/go-libp2p-core v0.9.0
+	github.com/libp2p/go-libp2p-discovery v0.5.1 // indirect
+	github.com/libp2p/go-libp2p-kad-dht v0.12.2
+	github.com/libp2p/go-libp2p-quic-transport v0.11.2 // indirect
+	github.com/libp2p/go-tcp-transport v0.2.4 // indirect
+	github.com/smartystreets/assertions v1.0.1 // indirect
+	github.com/testground/sdk-go v0.2.7
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ testplans/bitswap/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+
+	"github.com/testground/sdk-go/run"
+	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/sdk-go/sync"
+
+	bitswap "github.com/ipfs/go-bitswap"
+	"github.com/ipfs/go-bitswap/network"
+	block "github.com/ipfs/go-block-format"
+	datastore "github.com/ipfs/go-datastore"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+)
+
+var (
+	testcases = map[string]interface{}{
+		"speed-test": run.InitializedTestCaseFn(runSpeedTest),
+	}
+	readyState    = sync.State("ready")
+	doneState     = sync.State("done")
+	listen        = "/ip4/0.0.0.0/tcp/9000"
+	providerTopic = sync.NewTopic("provider", "")
+	blockTopic    = sync.NewTopic("provider", "")
+)
+
+func main() {
+	run.InvokeMap(testcases)
+}
+
+func runSpeedTest(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+	ctx := context.Background()
+	h, err := libp2p.New(ctx, libp2p.ListenAddrStrings())
+	if err != nil {
+		return err
+	}
+	kad, err := dht.New(ctx, h)
+	if err != nil {
+		return err
+	}
+	bstore := blockstore.NewBlockstore(datastore.NewMapDatastore())
+	ex := bitswap.New(ctx, network.NewFromIpfsHost(h, kad), bstore)
+
+	switch runenv.TestGroupID {
+	case "providers":
+		err = runProvide(ctx, runenv, h, bstore, ex)
+	case "requestors":
+		err = runRequest(ctx, runenv, h, bstore, ex)
+	default:
+		err = errors.New("unknown test group id")
+	}
+	return err
+}
+
+func runProvide(ctx context.Context, runenv *runtime.RunEnv, h host.Host, bstore blockstore.Blockstore, ex exchange.Interface) error {
+	tgc := sync.MustBoundClient(ctx, runenv)
+	addrs, err := h.Network().InterfaceListenAddresses()
+	if err != nil {
+		return err
+	}
+	// tell the requestors where I am reachable
+	for _, a := range addrs {
+		tgc.MustPublish(ctx, providerTopic, a.String())
+	}
+	// wait until they are ready
+	tgc.MustSignalAndWait(ctx, readyState, runenv.TestInstanceCount)
+
+	size := runenv.SizeParam("size")
+	runenv.RecordMessage("generating %s-sized random block", size)
+	buf := make([]byte, size)
+	rand.Read(buf)
+	blk := block.NewBlock(buf)
+	err = bstore.Put(blk)
+	if err != nil {
+		return err
+	}
+	err = ex.HasBlock(blk)
+	if err != nil {
+		return err
+	}
+	blkcid := blk.String()
+	runenv.RecordMessage("publishing block %s", blkcid)
+	tgc.MustPublish(ctx, blockTopic, blkcid)
+	return nil
+}
+
+func runRequest(ctx context.Context, runenv *runtime.RunEnv, h host.Host, bstore blockstore.Blockstore, ex exchange.Interface) error {
+	return nil
+}
+
+// size := runenv.SizeParam("size")
+// bandwidths := runenv.SizeArrayParam("bandwidths")
+// var latencies []time.Duration
+// lats := runenv.StringArrayParam("latencies")
+// for _, l := range lats {
+// 	d, err := time.ParseDuration(l)
+// 	if err != nil {
+// 		runenv.RecordCrash(err)
+// 		panic(err)
+// 	}
+// 	latencies = append(latencies, d)
+// }
+// bandwidths = append([]uint64{0}, bandwidths...)
+// latencies = append([]time.Duration{0}, latencies...)
+
+// initCtx.MustWaitAllInstancesInitialized(ctx)
+// runenv.RecordMessage("all instances running")
+
+// switch runenv.TestGroupID {
+// case "providers":
+// 	runenv.RecordMessage("I'm a provider, serving a %d size file", size)
+// case "requesters":
+// 	runenv.RecordMessage("I'm a requester")
+// }
+// return nil
+// }
